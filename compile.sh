5b70ba9a7b8082f12734fe2e4c12b1339b7556d7
ilmari-koria <ilmarikoria@posteo.net>
2024-11-29 18:29:44 +0000

    [sh] add progress

--- compile.sh
+++ compile.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-
 DIR_TEX="./lib/tex"
-DIR_PDF_OUTPUT="./publish"
+DIR_PUBLISH="./publish"
 DIR_TMP="./tmp"
 SAXON="./bin/saxon/saxon-he-12.4.jar"
 XML_PROMPTS="./lib/xml/prompts.xml"
-XSL_SPLIT="./lib/split-xml.xsl"
-XSL_TEX="./lib/prompt-xml-to-tex.xsl"
+XSL_SPLIT="./lib/xsl/split-xml.xsl"
+XSL_TEX="./lib/xsl/prompt-xml-to-tex.xsl"
 
 # split xml beginner and intermediate
-java -cp $SAXON net.sf.saxon.Transform -t -s:"$XML_PROMPTS" -xsl:"$XSL_SPLIT" -o:"$DIR_TMP/out"
+java -cp $SAXON net.sf.saxon.Transform -t -xsl:"$XSL_SPLIT" -s:"$XML_PROMPTS"
 
 # generate tex
-java -cp $SAXON net.sf.saxon.Transform -t -s:"$XML_BEGINNER" -xsl:"$XSL_TEX" -o:"$DIR_TMP/beginner.tex"
-java -cp $SAXON net.sf.saxon.Transform -t -s:"$XML_INTERMEDIATE" -xsl:"$XSL_TEX" -o:"$DIR_TMP/intermediate.tex"
+java -cp $SAXON net.sf.saxon.Transform -t -s:"$DIR_TMP/split/beginner.xml" -xsl:"$XSL_TEX" -o:"$DIR_TMP/tex/beginner.tex"
+# java -cp $SAXON net.sf.saxon.Transform -t -s:"$XML_INTERMEDIATE" -xsl:"$XSL_TEX" -o:"$DIR_TMP/tex/intermediate.tex"
 
-# generate pdf
-latexindent -w "$DIR_TMP/beginner.tex"
-latexindent -w "$DIR_TMP/intermediate.tex"
-xelatex -interaction=nonstopmode "$DIR_TMP/beginner.tex"
-xelatex -interaction=nonstopmode "$DIR_TMP/intermediate.tex"
+# # generate pdf
+latexindent -w "$DIR_TMP/tex/beginner.tex"
+xelatex -output-directory=$DIR_PUBLISH -interaction=nonstopmode "$DIR_TMP/tex/beginner.tex"
+# latexindent -w "$DIR_TMP/intermediate.tex"
+# xelatex -interaction=nonstopmode "$DIR_TMP/intermediate.tex"
 
-# mv and rm tmp files
-mv $DIR_TMP/*.pdf "$DIR_PDF_OUTPUT"
-rm $DIR_TMP/*.tex $DIR_TMP/*.log $DIR_TMP/*.out $DIR_TMP/*.bak* $DIR_TMP/*.aux
+# # mv and rm tmp files
+# rm $DIR_TMP/*.tex $DIR_TMP/*.log $DIR_TMP/*.out $DIR_TMP/*.bak* $DIR_TMP/*.aux
